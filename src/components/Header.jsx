4a688f34874c8f9d07d3cda33edeff8c85a160c0
Matias <matiasgc1207@gmail.com>
2024-09-20 11:07:58 -0500

    commit3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-blue-500 text-white p-6">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-3xl font-bold">Mi Agencia Web</h1>
+        <h1 className="text-3xl font-bold">Ecucommerce</h1>
         <nav>
           <ul className="flex space-x-6">
             <li><a href="#services" className="hover:underline">Servicios</a></li>
